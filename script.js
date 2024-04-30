083b53ba3e0818b2f6b0513ca38ac7a36800d1b6
Soumo275 <soumostudy27@gmail.com>
2024-05-01 00:13:59 +0530

    added colours

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const random = [];
+const all_value=[];
 
 function createbomb(){
 
@@ -6,9 +7,9 @@ function createbomb(){
 
     for (let i = 0; i < 10; i++) {
     
-    let num=Math.floor(Math.random() * 64);
+    let num=Math.floor(Math.random() * 49);
 
-    if( random.includes(num) ){
+    if( random.includes(num) && num==24){
         i--;
     }
     else
@@ -21,58 +22,24 @@ function createbomb(){
     }
 };
 
-
-function createboxes(){
-
-    var i,j;
-    var board=document.getElementById("board");
-    board.innerHTML="";
-    let c=0;
-    createbomb();
-
-    for(i=0;i<8;i++){
-        for(j=0;j<8;j++){
-
-                let bomb=random.includes(c);
-                if(bomb==false) bomb="";
-                else bomb="X";
-
-                let value=checkvalue(c,i,j);;
-                if(bomb=='X')
-                    value='';
-
-                
-
-                board.innerHTML=board.innerHTML + "<button value=' "+ c+ " ' onClick='check(this.value)'> "+bomb+value+"</button>";
-                c++;
-
-        }
-        board.innerHTML=board.innerHTML + "<br>";
-        
-
-    }
-
-};
-
 function checkvalue(c,i,j){
 
     let neighbour=[]
 
     if(i-1>=0){
-        neighbour.push(c-8);
-        if(j-1>=0)neighbour.push(c-8-1);
-        if(j+1<=7)neighbour.push(c-8+1);
+        neighbour.push(c-7);
+        if(j-1>=0)neighbour.push(c-7-1);
+        if(j+1<=6)neighbour.push(c-7+1);
     }
 
-    if(i+1<=7){
-        neighbour.push(c+8);
-        if(j-1>=0)neighbour.push(c+8-1);
-        if(j+1<=7)neighbour.push(c+8+1);
+    if(i+1<=6){
+        neighbour.push(c+7);
+        if(j-1>=0)neighbour.push(c+7-1);
+        if(j+1<=6)neighbour.push(c+7+1);
     }
-
 
     if(j-1>=0)neighbour.push(c-1);
-    if(j+1<=7)neighbour.push(c+1);
+    if(j+1<=6)neighbour.push(c+1);
     
 
     var count=0;
@@ -87,9 +54,54 @@ function checkvalue(c,i,j){
     else 
         return count;
 
-}
+    
 
-function check(v){
-    var x=document.getElementById('n');
-    x.innerHTML=v;
 };
+
+function reveal(tile){
+
+    var x=document.getElementById('n');
+    x.innerHTML=tile;
+    
+    document.getElementById(tile).innerHTML=all_value[tile];
+    document.getElementById(tile).style.backgroundColor='lightgreen';
+    document.getElementById(tile).style.color='black';
+    
+
+};
+
+
+function createboxes(){
+
+    var i,j;
+    var board=document.getElementById("board");
+    board.innerHTML="";
+    let c=0;
+    createbomb();
+
+    for(i=0;i<7;i++){
+        for(j=0;j<7;j++){
+
+                let bomb=random.includes(c);
+                if(bomb==false) bomb="";
+                else bomb="X";
+
+                let value=checkvalue(c,i,j);
+
+                if(bomb=='X')
+                    all_value.push('X');
+                else
+                    all_value.push(value)
+
+                
+
+
+               board.innerHTML += "<button id=" + c + " onClick='reveal(this.id)'> </button>";
+                c++;
+
+        }
+        board.innerHTML=board.innerHTML + "<br>";
+        
+    }
+};
+
